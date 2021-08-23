33715591bd58c141fd468c0c9816cf7101220bad
Val Currie <contact@va.lcuri.email>
2021-08-23 15:32:29 -0500

    brazil customer invoices

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,8 @@
+SELECT 
+	FirstName ||''|| LastName AS FullName, 
+	InvoiceId,
+	InvoiceDate,
+	BillingCountry 
+FROM Customer 
+JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
+WHERE BillingCountry = "Brazil";
